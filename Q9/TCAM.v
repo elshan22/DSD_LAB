c588694193c5d9d3ebb5f01abea0e480e9261ee2
aylasalehi <79265151+aylasalehi@users.noreply.github.com>
2022-09-01 05:58:27 +0430

    edit

--- Q9/TCAM.v
+++ Q9/TCAM.v
@@ -1,13 +1,30 @@
 `timescale 1ns / 1ps
-module TCAM #(parameter WORD_WIDTH = 8, parameter MEMORY_SIZE = 20)
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    02:40:52 09/01/2022 
+// Design Name: 
+// Module Name:    TCAM 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module TCAM #(parameter WORD_WIDTH = 16, parameter MEMORY_SIZE = 32)
 				 (input [WORD_WIDTH-1: 0] A, K,
 				  input [$clog2(MEMORY_SIZE)-1: 0] write_addr,
 				  input read, write, reset, clk,
 				  output [MEMORY_SIZE-1: 0] matched);
 	
 	reg [2*WORD_WIDTH-1: 0] memory [MEMORY_SIZE-1: 0];
-	
-	reg [2*WORD_WIDTH-1: 0] search_reg;
 	
 	reg enable;
 	
@@ -23,7 +40,6 @@ module TCAM #(parameter WORD_WIDTH = 8, parameter MEMORY_SIZE = 20)
 	always @(posedge clk or negedge reset) begin
 		if (!reset) begin
 			enable = 1'b0;
-			search_reg = 0;
 			for (j = 0; j < MEMORY_SIZE; j=j+1)
 				memory[j] = {{WORD_WIDTH{1'b1}}, {WORD_WIDTH{1'b0}}};
 		end else if (read)	enable = 1'b1;
@@ -33,4 +49,4 @@ module TCAM #(parameter WORD_WIDTH = 8, parameter MEMORY_SIZE = 20)
 			 end
 	end
 	
-endmodule
+endmodule 
